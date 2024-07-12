e46cad40cadd407b0291b01cbf1ade365d7f472e
jexhsu <jexhsu@gmail.com>
2024-07-12 18:48:30 +0800

    update

--- public/app.js
+++ public/app.js
@@ -15,15 +15,5 @@ const greetPerson = (person) => {
     console.log('hello ', person.name);
 };
 greetPerson(me);
+export {};
 //greetPerson({name: 'shaun'});
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-// inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
--- src/app.ts
+++ src/app.ts
@@ -25,27 +25,7 @@ me.speak('hello, world');
 
 const greetPerson = (person: IsPerson): void => {
   console.log('hello ', person.name);
-}
+};
 
 greetPerson(me);
 //greetPerson({name: 'shaun'});
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
-
-// inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
-
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
-});
