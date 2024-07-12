38326d9301bb981c79dad2eff5a898a48ca7a276
jexhsu <jexhsu@gmail.com>
2024-07-12 16:13:40 +0800

    wip: update

--- public/sandbox.js
+++ public/sandbox.js
@@ -3,11 +3,11 @@
 // example 1
 var greet;
 greet = function (name, greeting) {
-    console.log(name + " says " + greeting);
+    console.log("".concat(name, " says ").concat(greeting));
 };
 // example 2
-var calc;
-calc = function (numOne, numTwo, action) {
+var calc1;
+calc1 = function (numOne, numTwo, action) {
     if (action === 'add') {
         return numOne + numTwo;
     }
@@ -18,5 +18,30 @@ calc = function (numOne, numTwo, action) {
 // example 3
 var logDetails;
 logDetails = function (ninja) {
-    console.log(ninja.name + " is " + ninja.age + " years old");
+    console.log("".concat(ninja.name, " is ").concat(ninja.age, " years old"));
 };
+var operations = {
+    add: function (a, b) { return a + b; },
+    subtract: function (a, b) { return a - b; },
+    multiply: function (a, b) { return a * b; },
+    divide: function (a, b) {
+        if (b !== 0) {
+            return a / b;
+        }
+        else {
+            throw new Error('Division by zero is not allowed');
+        }
+    },
+};
+var calc2;
+calc2 = function (numOne, numTwo, action) {
+    var operation = operations[action];
+    console.log(operation);
+    if (operation) {
+        return operation(numOne, numTwo);
+    }
+    else {
+        throw new Error('Unsupported action');
+    }
+};
+console.log(calc2(10, 5, 'ad'));
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -5,22 +5,52 @@ let greet: (a: string, b: string) => void;
 
 greet = (name: string, greeting: string) => {
   console.log(`${name} says ${greeting}`);
-}
+};
 
 // example 2
-let calc: (a: number, b: number, c: string) => number;
+let calc1: (a: number, b: number, c: string) => number;
 
-calc = (numOne: number, numTwo: number, action: string) => {
+calc1 = (numOne: number, numTwo: number, action: string) => {
   if (action === 'add') {
     return numOne + numTwo;
   } else {
     return numOne - numTwo;
   }
-}
+};
 
 // example 3
-let logDetails: (obj: {name: string, age: number}) => void;
+let logDetails: (obj: { name: string; age: number }) => void;
 
-logDetails = (ninja: {name: string, age: number}) => {
+logDetails = (ninja: { name: string; age: number }) => {
   console.log(`${ninja.name} is ${ninja.age} years old`);
-}
+};
+
+// example 4
+type Calculation = (a: number, b: number) => number;
+
+const operations: { [key: string]: Calculation } = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b !== 0) {
+      return a / b;
+    } else {
+      throw new Error('Division by zero is not allowed');
+    }
+  },
+};
+
+let calc2: (a: number, b: number, c: string) => number;
+
+calc2 = (numOne, numTwo, action) => {
+  const operation = operations[action];
+  console.log(operation);
+  if (operation) {
+    return operation(numOne, numTwo);
+  } else {
+    throw new Error('Unsupported action');
+  }
+};
+
+console.log(calc2(10, 5, 'ad'));
