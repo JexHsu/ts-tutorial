ed494bebf2a3c89b218c99dca7b0a9d7c09f3656
jexhsu <jexhsu@gmail.com>
2024-07-12 18:52:21 +0800

    update

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ var Invoice = /** @class */ (function () {
         this.amount = a;
     }
     Invoice.prototype.format = function () {
-        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+        return "".concat(this.client, " owes \u00A3").concat(this.amount, " for ").concat(this.details);
     };
     return Invoice;
 }());
@@ -18,14 +18,3 @@ invoices.push(invOne);
 invoices.push(invTwo);
 // invoices.push({ name: 'shaun' });
 console.log(invoices);
-var form = document.querySelector('.new-item-form');
-console.log(form.children);
-// inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
